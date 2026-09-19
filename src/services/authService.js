56fbac8737ebcb39e121ae082ecb49b9ca1741d4
Deekshith1809 <149555762+Deekshith1809@users.noreply.github.com>
2026-09-19 21:54:48 +0530

    updated

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api/auth';
+const API_BASE_URL =  'https://kiranavoice-inventory.onrender.com';
 
 function getAuthHeader() {
   const token = localStorage.getItem('auth_token');
